101bd547f2829796614f6301069d4890af4a2cd0
Lawrence <lawrenceowino96@gmail.com>
2025-08-31 06:22:04 -0700

    Implement custom configurations to display title, author, and publication_year in the admin list view. Configure list filters and search capabilities to enhance the admin’s usability for Book entries.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")  # columns shown in the list view
+    list_filter = ("publication_year", "author")  # filters on the right-hand side
+    search_fields = ("title", "author")  # search box for quick lookups
+
+
+# Register the model with the custom admin
+admin.site.register(Book, BookAdmin)
