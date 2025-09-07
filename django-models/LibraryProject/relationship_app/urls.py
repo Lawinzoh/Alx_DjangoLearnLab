627c3bda2fe74eeca0cd25837737f48822aee446
Lawrence <lawrenceowino96@gmail.com>
2025-09-07 03:47:08 -0700

    Implementing User Authentication in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 from .views import SignUpView, list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -5,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', list_books, name='list_books'), # Function based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # Class based view
-    path("register/", SignUpView.as_view(), name="register"), # User registration
+    path("register/", views.register, name="register"), # User registration
     path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"), # User login
-    path("logout/", LogoutView.as_view(next_page="login"), name="logout"), # User logout
+    path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"), # User logout
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Library
 from Introduction_to_Django.LibraryProject.bookshelf.models import Book
 from django.contrib.auth import login
@@ -26,8 +26,20 @@ class LibraryDetailView(DetailView):
         context['books'] = self.object.books.all()
         return context
     
-#User Registration
+#User Registration--class based view
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+#User Registration--function based view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in the new user immediately
+            return redirect("list_book")  # redirect anywhere in your app
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
