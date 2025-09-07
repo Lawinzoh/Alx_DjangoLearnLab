75121fd34582f67a5dce8ccabac51a47dbd2f713
Lawrence <lawrenceowino96@gmail.com>
2025-09-07 02:40:17 -0700

    Create model Author, Create model Book, Create model Library,  Create model Librarian. Query all books by a specific author, List all books in a library, Retrieve the librarian for a library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
     try:
-        author = Author.objects.get(name=author_name)
+        author = Author.objects.filter(name=author_name)
         books = author.books.all()
         return books
     except Author.DoesNotExist:
