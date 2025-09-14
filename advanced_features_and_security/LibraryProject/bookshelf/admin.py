20fa6eff7f6270007e419c1968493a7c37acffad
Lawrence <lawrenceowino96@gmail.com>
2025-09-14 06:28:55 -0700

    feat(auth): integrate CustomUser model across project
    - Updated UserProfile to reference AUTH_USER_MODEL
    - Refactored Book and Library models to use CustomUser for relations
    - Replaced direct User imports with get_user_model() in views and forms
    - Ensured consistency across app with custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile
 
 # Register your models here.
 
@@ -11,3 +13,18 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register the model with the custom admin
 admin.site.register(Book, BookAdmin)
+
+class ModelAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser") # Fields displayed in the list view
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}), 
+    )                                                              # Fields available when editing/creating a user
+    # Fields available when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+# Register both the custom user and the UserProfile
+admin.site.register(CustomUser, ModelAdmin)
+admin.site.register(UserProfile)
